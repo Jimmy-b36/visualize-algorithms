1750ab8d7fc8df22e4b9fabaf25d90487732628c
jimmy_btv <jimmy.btv22@gmail.com>
2022-10-19 18:58:30 -0700

    feat: more robust testing

---
+++ __tests__/sortingTest.ts
@@ -0,0 +1,35 @@
+const mergeSort = require('../helpers/sorting').mergeSort;
+const selectionSort = require('../helpers/sorting').selectionSort;
+const bubbleSort = require('../helpers/sorting').bubbleSort;
+
+const data: number[][] = [];
+for (let i = 0; i < 10; i++) {
+  data.push(
+    Array.from({ length: Math.floor(Math.random() * 40) }, () =>
+      Math.floor(Math.random() * 40)
+    )
+  );
+}
+test('selectionSort', () => {
+  data.forEach((item: number[]) => {
+    expect(selectionSort(item)).toEqual(item.sort((a, b) => a - b));
+  });
+});
+
+test('bubbleSort', () => {
+  data.forEach((item: number[]) => {
+    expect(bubbleSort(item)).toEqual(item.sort((a, b) => a - b));
+  });
+});
+
+test('mergeSort', () => {
+  data.forEach((item: number[]) => {
+    expect(mergeSort(item)).toEqual(item.sort((a, b) => a - b));
+  });
+});
+
+// test('insertionSort', () => {
+//   const data: number[] = [24, 76, 10, 73, 18];
+//   expect(insertionSort(data)).toEqual([10, 18, 24, 73, 76]);
+// });
+export {};
